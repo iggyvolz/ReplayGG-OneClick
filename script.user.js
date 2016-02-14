ce248a9d4610628982ce40f359872d3e631570c3
Ryan Volz <iggyvolz@gmail.com>
2016-02-14 16:32:14 -0500

    Don't circumvent survey

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replay.gg OneClick
 // @namespace    http://www.replay.gg
-// @version      0.1
+// @version      0.2
 // @description  Click-to-run on Replay.gg
 // @author       iggyvolz
 // @include      http://www.replay.gg/*
@@ -9,9 +9,21 @@
 // ==/UserScript==
 if(window.location.pathname.indexOf("/search/na/")===0)
 {
-    $("a[data-target=#modal-view-replay]").click(function(a) {window.location="rgg://"+a.currentTarget.dataset.gameId;return false})
+    var oldGet=jQuery.get
+    jQuery.get=function(a,c,d,e) {
+        if(a.indexOf("http://www.replay.gg/ajax/replay/")===0)
+        {
+            var id=a.split("/")[5]
+            alert("rgg://"+id)
+            $("#modal-view-replay button[data-dismiss=modal].close").click()
+        }
+        else
+        {
+            return oldGet(a,c,d,e)
+        }
+    }
 }
 else if(window.location.pathname==="/")
 {
-    $($(".row")[1]).html($($(".row")[1]).html()+"<div class=\"col-xs-12 text-center\">Running Replay.GG OneClick version 0.1 by Iggyvolz</div>")
+    $($(".row")[1]).html($($(".row")[1]).html()+"<div class=\"col-xs-12 text-center\">Running Replay.GG OneClick version 0.2 by Iggyvolz</div>")
 }
